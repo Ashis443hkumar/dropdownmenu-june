e89ae21ae66670080ac3ebdc273fe500abf7abf8
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-06-02 00:38:09 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./App.css"
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Outlet } from 'react-router-dom';
 
 function Home() {
+  console.log(h1)
   return (
     <div>
       <h1 className="home">Home</h1>
+      <Outlet/>
+      
     </div>
   );
 }
